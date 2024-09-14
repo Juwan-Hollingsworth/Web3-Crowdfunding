b8a42c9e48f3beacc4a9b918e5b06087321bfe94
Juwan-Hollingsworth <jhollingsworth5@student.clayton.edu>
2024-09-13 21:53:41 -0400

    add conditional for inactive icon

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { navlinks } from '../constants'
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'}flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
-  {!isActive ? (  <img src={imgUrl} alt="fund logo" className='w-1/2 h-1/2'/>): (  <img src={imgUrl} alt="fund logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`}/>)}
+    {!isActive ? (<img src={imgUrl} alt="fund logo" className='w-1/2 h-1/2' />) : (<img src={imgUrl} alt="fund logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />)}
 
 
   </div>
