3b6c0e832e835ae3c91e5121a5990a08665bc973
Juwan-Hollingsworth <jhollingsworth5@student.clayton.edu>
2024-09-13 21:22:34 -0400

    Application layout completed.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
+import { RouterTypeInput } from '@thirdweb-dev/react'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {CampaignDetails, CreateCampaign, Home, Profile} from './pages'
+import {Sidebar, Navbar} from './components'
 
 const App = () => {
   return (
-    <div>
-      App 
+    //create the layout for the application
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10 relative'>
+      <Sidebar/>
+      </div>
+
+    <h1 className='font-xl font-bold'>Test</h1>
+
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+      <Navbar/>
+      </div>
+
+      {/* specify the routes */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
+
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
-import App from './App'
 
+import App from './App'
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 //wrappper for the entire application
