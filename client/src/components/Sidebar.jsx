fa32b5ed95cf85568a6b09835c94ac73609bf752
Juwan-Hollingsworth <jhollingsworth5@student.clayton.edu>
2024-09-28 16:19:27 -0400

    implement sidebar and navigation on client side

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,10 +4,12 @@ import { logo, sun } from '../assets/assets'
 import { navlinks } from '../constants'
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'}flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
-    {!isActive ? (<img src={imgUrl} alt="fund logo" className='w-1/2 h-1/2' />) : (<img src={imgUrl} alt="fund logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />)}
-
-
+  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+    {!isActive ? (
+      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+    ) : (
+      <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
+    )}
   </div>
 )
 
@@ -22,6 +24,26 @@ const Sidebar = () => {
       <Link to={"/"}>
         <Icon styles='w-[52px] h-[52px] bg-[#2c2f32]' imgUrl={logo} />
       </Link>
+
+      <div className='flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rouned-[20px] w-[76px] py-4 mt-12 '>
+      <div className='flex flex-col justify-center items-center gap-3'>
+        {navlinks.map((link)=>(
+          <Icon 
+          key={link.name}
+          {...link}
+          isActive={isActive}
+          handleClick={()=>{
+            if(!link.disabled){
+              setIsActive(link.name)
+              navigate(link.link)
+            }
+          }} 
+          />
+        ))}
+      </div>
+      <Icon styles={"bg-[#1c1c24] shadow-secondary"} imgUrl={sun}/>
+
+      </div>
 
     </div>
   )
